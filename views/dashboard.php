59198bf042f18b2db8f8eb798e74d2c9f2e537e9
RARE GRADE TECH <raregradetech@gmail.com>
2025-11-21 10:52:51 +0200

    Create dashboard.php

---
+++ views/dashboard.php
@@ -0,0 +1,10 @@
+<?php init_head(); ?>
+<div class="content">
+  <div class="row">
+     <div class="col-md-12">
+        <h4><?php echo _l('aa_dashboard'); ?></h4>
+        <p>Welcome to Advanced Accounting. More widgets coming soon.</p>
+     </div>
+  </div>
+</div>
+<?php init_tail(); ?>
