d5ca3bcd37a10f4682c5b85f5b1bd7807e708ca5
RARE GRADE TECH <raregradetech@gmail.com>
2025-11-21 10:48:10 +0200

    Create advanced_accounting_lang.php

---
+++ language/english/advanced_accounting_lang.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['aa_menu']               = 'Advanced Accounting';
+$lang['aa_dashboard']          = 'Dashboard';
+$lang['aa_chart_of_accounts']  = 'Chart of Accounts';
+$lang['aa_reports']            = 'Reports';
+$lang['aa_profit_loss']        = 'Profit & Loss';
+$lang['aa_settings']           = 'Settings';
+$lang['aa_permission_view']    = 'View';
+$lang['aa_permission_manage']  = 'Manage';
+$lang['aa_account_code']       = 'Code';
+$lang['aa_account_name']       = 'Account Name';
+$lang['aa_type']               = 'Type';
+$lang['aa_income']             = 'Income';
+$lang['aa_expenses']           = 'Expenses';
+$lang['aa_profit']             = 'Profit';
+$lang['settings_updated']      = 'Settings updated successfully';
+
